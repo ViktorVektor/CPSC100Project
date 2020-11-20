cae4df0dc2acc0efd649c189ca9225c35c20f107
ViktorVektor <41247872+ViktorVektor@users.noreply.github.com>
2020-11-19 19:58:49 -0800

    main UI

---
+++ UI/main.py
@@ -0,0 +1,16 @@
+import pygame, math, time, random
+
+# creating the main menu for the program
+
+pygame.init()
+
+width = 1280
+height = 720
+
+# colour 
+GRAY = (170, 170 , 170)
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
